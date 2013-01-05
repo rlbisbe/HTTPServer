13349895dc91f2ec98db1329c9cc47ace21ec987
rlbisbe <roberto.luis@rlbisbe.net>
2013-01-05 01:24:44 +0100

    More refactoring.

--- HTTPServer/Program.cs
+++ HTTPServer/Program.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Sockets;
-using System.Net;
-using System.IO;
-
-namespace HTTPServer
+﻿namespace HTTPServer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            WebServer server = new WebServer("127.0.0.1", 5000);
+            WebServer server = new WebServer(5000);
             server.Start();
             server.Listen();
         }
